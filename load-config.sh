2cc32b6988b580173da81c17e85d0a39f62bfea1
eggydutch <59375032+eggydutch@users.noreply.github.com>
2022-07-14 21:26:46 +0800

    Update load-config.sh

--- load-config.sh
+++ load-config.sh
@@ -20,14 +20,14 @@ sed -i '$d' package/base-files/files/bin/config_generate
 # sed -i '$a uci set network.wwan.up=1' package/base-files/files/bin/config_generate
 # sed -i "19a \ \ \ \ \ \ \ \ list   network          'wwan' " package/network/config/firewall/files/firewall.config
 
-# 设置风扇30度起转
-echo "uci set glfan.@globals[0].temperature='30'" >> package/base-files/files/bin/config_generate
+# 设置风扇50度起转
+echo "uci set glfan.@globals[0].temperature='50'" >> package/base-files/files/bin/config_generate
 echo "uci set glfan.@globals[0].intergration='4'" >> package/base-files/files/bin/config_generate
 echo "uci set glfan.@globals[0].differential='20'" >> package/base-files/files/bin/config_generate
 echo "uci commit glfan" >> package/base-files/files/bin/config_generate
 
-# 设定内网默认ip为192.168.8.1
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+# 设定内网默认ip为192.168.1.20
+sed -i 's/192.168.1.1/192.168.1.20/g' package/base-files/files/bin/config_generate
 
 # 修改主机名
 sed -i 's/OpenWrt/GL-AXT1800/' package/base-files/files/bin/config_generate
